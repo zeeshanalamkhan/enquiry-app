560bb1278cc2e337e030448207a15693ff43a786
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-09-20 19:51:44 +0530

    InstituteDTO class

---
+++ enquiry-app/src/main/java/com/zeeshan/dto/InstituteDto.java
@@ -0,0 +1,15 @@
+package com.zeeshan.dto;
+
+public interface InstituteDto {
+
+	public Integer getInstituteId();
+
+	public String getName();
+
+	public String getPhone();
+
+	public String getEmail();
+
+	public String getCity();
+
+}
