3c2100b26f55ca3acba7c7307aa119f759d4a289
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-09-20 19:28:04 +0530

    SpringBootApplication class

---
+++ enquiry-app/src/main/java/com/zeeshan/EnquiryAppApplication.java
@@ -0,0 +1,13 @@
+package com.zeeshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnquiryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnquiryAppApplication.class, args);
+	}
+
+}
