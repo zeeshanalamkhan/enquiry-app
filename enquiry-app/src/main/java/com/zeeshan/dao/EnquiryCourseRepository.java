3c1ad9addf9467b79ba47cbae254455b60c8bd40
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-09-20 19:46:02 +0530

    EnquiryCourseRepository class

---
+++ enquiry-app/src/main/java/com/zeeshan/dao/EnquiryCourseRepository.java
@@ -0,0 +1,9 @@
+package com.zeeshan.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zeeshan.domain.EnquiryCourse;
+
+public interface EnquiryCourseRepository extends JpaRepository<EnquiryCourse, Long> {
+
+}
